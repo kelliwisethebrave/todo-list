080e5c6148e9e0f07535d5f98ffa6ff706379756
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2025-11-06 20:26:26 -0700

    Complete up to Part 4: Final Steps in Week 1 Lesson Assignment.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -10,9 +11,20 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      react.configs.recommended,
+      react.configs['jsx-runtime'],
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    settings: { 
+      react: { 
+        version: 'detect', 
+      }, 
+    },
+    plugins: {
+      react,
+      reactHooks,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,7 +35,12 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      //'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],//
+      'no-unused-vars': 'warn', //this changes the error to a warning
+      'react/prop-types': 'off', //this suppresses warnings about not using prop-type
     },
   },
 ])
