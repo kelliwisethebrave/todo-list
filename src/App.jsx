8ea3822f0f6fa2b20783ea1deb74465cbf9c8c90
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2025-12-09 21:52:39 -0700

    Add TodoListItem and updates to App & TodoList to use TodoListItem as a component, complete week 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import TodoList from './TodoList'
-import TodoForm from './TodoForm'
+import './App.css';
+import { useState } from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
-
+  const [newTodo, setNewTodo] = useState('this is example text');
   return (
-      <div>
-        <h1>My ToDos</h1>
-        <TodoForm />
-        <TodoList />
-
-      </div>
-  )    
+    <div>
+      <h1>My ToDos</h1>
+      <TodoForm />
+      <p>{newTodo}</p>
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,22 @@
-function TodoList(){
-    const todos = [
-  {id: 1, title: "review resources"},
-  {id: 2, title: "take notes"},
-  {id: 3, title: "code out app"},
-]
-    return (
-      <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
-    );
+import TodoListItem from './TodoListItem';
+
+function TodoList() {
+  const todos = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+      {/* the below is the original arrow function before being 
+      converted to being used with the TodoListItem component
+      
+      <{todos.map(todo => <li key={todo.id}>{todo.title}</li>)}>*/}
+    </ul>
+  );
 }
 
-export default TodoList
+export default TodoList;
