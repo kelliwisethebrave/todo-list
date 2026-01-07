e2f07c86e51e4106cf6d31cb80e7a671989aad99
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2026-01-06 18:28:19 -0700

    Update for Week 6, converting to controlled components and conditional rendering

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,23 +1,34 @@
 import TodoListItem from './TodoListItem';
 
-function TodoList({ todoList }) {
-  {
-    /*const todos = [
+function TodoList({ todoList, onCompleteTodo }) {
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
+
+  /*const todos = [
     { id: 1, title: 'review resources' },
     { id: 2, title: 'take notes' },
     { id: 3, title: 'code out app' },
   ];*/
-  }
+
   return (
-    <ul>
-      {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
-      ))}
+    <>
+      {filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+      ) : (
+        <ul>
+          {filteredTodoList.map((todo) => (
+            <TodoListItem
+              key={todo.id}
+              todo={todo}
+              onCompleteTodo={onCompleteTodo}
+            />
+          ))}
+        </ul>
+      )}
       {/* the below is the original arrow function before being 
       converted to being used with the TodoListItem component
       
       <{todos.map(todo => <li key={todo.id}>{todo.title}</li>)}>*/}
-    </ul>
+    </>
   );
 }
 
